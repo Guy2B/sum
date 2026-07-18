25d416be745ceb14d3887f869acdd03659975b3f
Guy2B <guybeaho@gmail.com>
2026-07-18 14:02:17 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 'use strict';
 window.SIGMA_FIREBASE_CONFIG = Object.freeze({
-  apiKey: 'AIzaSyDN2I3We8eDMI5WvqMH5Piu_7GrftPZpdY',
-  authDomain: 'project-sum-b961a.firebaseapp.com',
-  projectId: 'project-sum-b961a',
-  storageBucket: 'project-sum-b961a.firebasestorage.app',
-  messagingSenderId: '669651136797',
-  appId: '1:669651136797:web:6fc477089138712e7031f4'
+  apiKey: "AIzaSyDN2I3We8eDMI5WvqMH5Piu_7GrftPZpdY",
+  authDomain: "project-sum-b961a.firebaseapp.com",
+  projectId: "project-sum-b961a",
+  storageBucket: "project-sum-b961a.firebasestorage.app",
+  messagingSenderId: "669651136797",
+  appId: "1:669651136797:web:6fc477089138712e7031f4",
+  functionsRegion: 'europe-west1'
 });
