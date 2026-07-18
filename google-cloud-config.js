e9f0e7f922e4396734641c21e96bc9524b8c8f6a
Guy2B <guybeaho@gmail.com>
2026-07-18 22:44:36 +0200

    Update google-cloud-config.js

--- google-cloud-config.js
+++ google-cloud-config.js
@@ -1,7 +1,7 @@
 'use strict';
 window.SIGMA_GOOGLE_CLOUD_CONFIG = Object.freeze({
   // Public OAuth 2.0 Web Client ID only. Never place the client secret here.
-  oauthClientId: 'REPLACE_WITH_NEW_GOOGLE_OAUTH_WEB_CLIENT_ID.apps.googleusercontent.com',
+  oauthClientId: '669651136797-lv4f7hnd053shmn128i9phess4sigs85.apps.googleusercontent.com',
   // Optional Google Apps Script /exec URL used as a Gemini proxy.
   appsScriptAiProxyUrl: '',
   gmailMaxMessages: 30,
